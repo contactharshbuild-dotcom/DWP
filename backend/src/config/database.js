8f22e158065112a5f0ae96aff943a70c92d0c995
pratik bokade <pratikbokade786@gmail.com>
2026-08-09 13:00:15 +0530

    data

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,11 +2,13 @@ import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+  console.log(process.env.DB_PASSWORD)
 const sequelize = new Sequelize (
+  
 process.env.DB_NAME,
 process.env.DB_USER,
 process.env.DB_PASSWORD,
+
 //harsh
 {
     host: process.env.DB_HOST,
